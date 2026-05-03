0f7603c2792ef82784fdf8449f9f780ff0025dca
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2026-05-03 20:28:32 -0300

    fix: import missing class

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,4 @@
+require_relative 'parser/early'
 require_relative 'gramatica'
 
 if ARGV.empty?
@@ -7,5 +8,10 @@ end
 
 expressao = ARGV[0]
 tokens = expressao.gsub(/\s+/, '').split('')
-analisador = AnalisadorEarley.new($gramatica, 'S')
-analisador.analisar(tokens)
+
+analisador_earley = AnalisadorEarley.new(Gramatica.new, tokens[0])
+analisador_earley.analisar(tokens)
+
+# analisador_chumsky = AnalisadorChumsky.new(Gramatica.new, tokens[0])
+# analisador_chumsky.analisar(tokens)
+
