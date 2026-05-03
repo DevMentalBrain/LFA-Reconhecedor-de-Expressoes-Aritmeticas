194aadf65b88b50bc7528edf264b49e5fc2fa2d6
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2026-05-03 20:08:49 -0300

    feat: create main to execute parser via cli

--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,11 @@
-teste
+require_relative 'gramatica'
+
+if ARGV.empty?
+  puts "Uso: ruby src/main.rb \"expressao\""
+  exit
+end
+
+expressao = ARGV[0]
+tokens = expressao.gsub(/\s+/, '').split('')
+analisador = AnalisadorEarley.new($gramatica, 'S')
+analisador.analisar(tokens)
